bda24ea2ccfca6faedfce1c628ee4ee70a8b1a3a
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-28 19:19:56 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+const taskForm = document.querySelector("#create-task-form");
+
+const taskList = document.querySelector("#tasks");
+
+document.addEventListener("submit", (event) => {
   // your code here
+  event.preventDefault();
+
+  const newTask = document.querySelector("#new-task-description").value;
+
+  taskList.innerHTML += `<li> ${newTask} 
+    <button data-action="delete"> X </button>
+    </li>`;
+
+  taskForm.reset();
+});
+
+taskList.addEventListener("click", function (e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove();
+  }
 });
